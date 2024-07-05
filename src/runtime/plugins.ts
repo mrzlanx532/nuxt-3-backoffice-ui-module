cf0bf6e199a76eef78a079cd3d493af6973a715d
Denis Danilov <d.danilov@duotek.ru>
2024-07-05 23:38:32 +0300

    Add vue-3-click-outside plugin #4

--- src/runtime/plugins.ts
+++ src/runtime/plugins.ts
@@ -1,12 +1,35 @@
-import { defineNuxtPlugin } from '#imports'
+import { defineNuxtPlugin, onNuxtReady } from '#imports'
 
 import NotificationManager from './plugins/notification'
 import ModalManager from './plugins/modal'
 
 import * as vClickOutside from 'click-outside-vue3'
 
-export default defineNuxtPlugin((_nuxtApp) => {
-  _nuxtApp.vueApp.use(vClickOutside)
+import '@mrzlanx532/nuxt-3-custom-scroll-plugin/dist/css/common.css'
+import '@mrzlanx532/nuxt-3-custom-scroll-plugin/dist/css/v-scrollable.css'
+import '@mrzlanx532/nuxt-3-custom-scroll-plugin/dist/css/document-scroll.no-ssr.css'
+
+import { Scrollable, DocumentScroll } from '@mrzlanx532/nuxt-3-custom-scroll-plugin'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(vClickOutside)
+  nuxtApp.vueApp.directive('scrollable', {
+    mounted: function (el, binding) {
+      new Scrollable(el, binding.value)
+    }
+  })
+
+  onNuxtReady(async () => {
+    window.documentScroll = new DocumentScroll
+
+    try {
+      new ResizeObserver(() => {
+        window.documentScroll.updateScroll()
+      }).observe(document.documentElement)
+    } catch (e) {
+      console.warn('Не удалось установить ResizeObserver для document')
+    }
+  })
 
   return {
     provide: {
