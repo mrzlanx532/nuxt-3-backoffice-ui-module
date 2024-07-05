b89671520f3bd3a1d7c4aa1d84c84ec168517ff6
Denis Danilov <d.danilov@duotek.ru>
2024-07-05 15:56:38 +0300

    Add vue-3-click-outside plugin

--- src/runtime/plugins.ts
+++ src/runtime/plugins.ts
@@ -3,7 +3,11 @@ import { defineNuxtPlugin } from '#imports'
 import NotificationManager from './plugins/notification'
 import ModalManager from './plugins/modal'
 
-export default defineNuxtPlugin(() => {
+import vClickOutside from 'click-outside-vue3'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(vClickOutside)
+
   return {
     provide: {
       notification: new NotificationManager(),
