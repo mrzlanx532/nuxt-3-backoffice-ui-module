d3012f31d85abf413d8250ee02b8567aa8c684e7
Denis Danilov <d.danilov@duotek.ru>
2024-12-13 23:31:11 +0300

    Minor

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,9 +2,12 @@ export default defineNuxtConfig({
   modules: ['../src/module'],
   myModule: {},
   devtools: { enabled: true },
+
   app: {
     head: {
       title: 'Backoffice UI',
     }
   },
+
+  compatibilityDate: '2024-12-13',
 })
