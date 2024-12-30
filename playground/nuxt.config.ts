806e44004ed13e65b0d824d5d0a6081f3ce2d3a7
mrzlanx532 <denis_danilov_97@mail.ru>
2024-12-31 02:56:30 +0300

    Added auth module to playground

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,12 +1,12 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
-  myModule: {},
+  // myModule: {},
   devtools: { enabled: true },
 
   app: {
     head: {
       title: 'Backoffice UI',
-    }
+    },
   },
 
   compatibilityDate: '2024-12-13',
