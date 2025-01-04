be2a167a8d59d467451e4026003729af1f4ed6ba
Denis Danilov <d.danilov@duotek.ru>
2025-01-04 22:50:59 +0300

    No message

--- src/runtime/components/base/Detail.vue
+++ src/runtime/components/base/Detail.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import BaseLink from '../base/BaseLink.vue'
 import { type IConfigItem, type IItem } from '../../composables/useBrowser'
-import type { Component } from 'vue'
+import { type Component, toRaw } from 'vue'
 import { useBrowser } from '../../composables/useBrowser'
 import Badge from '../../../runtime/components/base/Badge.vue'
 
@@ -62,13 +62,13 @@ const props = withDefaults(defineProps<{
           <div class="clouds__cloud-row-value">
             <component
                 v-if="row.component && isVueComponent(row.component)"
-                :is="row.component"
+                :is="toRaw(row.component)"
                 :item="item"
                 :column="row"
             />
             <component
                 v-else-if="row.component && isVueComponent(row.component.component) && item[row.name] !== undefined && item[row.name] !== null"
-                :is="getSubComponent(row.component)"
+                :is="getSubComponent(toRaw(row.component))"
                 :item="item"
                 :column="row"
             />
