976d11523222826c8b73fef8b23719f07d95a2e4
Denis Danilov <d.danilov@duotek.ru>
2025-01-04 22:37:46 +0300

    No message

--- playground/pages/users/[id].vue
+++ playground/pages/users/[id].vue
@@ -4,6 +4,7 @@ import { useRoute, usePage } from '#imports'
 import Detail from '../../../src/runtime/components/base/Detail.vue'
 import Button from '../../../src/runtime/components/base/Button.vue'
 import UserForm from '../../modals/users/UserForm.vue'
+import Badge from '../../../src/runtime/components/base/Browser/ColumnComponents/Badge.vue'
 
 const route = useRoute()
 
@@ -17,8 +18,153 @@ const {
   SSRLoadDetail
 } = usePage()
 
-const sections = ref([
+const infoSection = {
+  title: 'Инфо',
+  rows: [
+    {
+      title: 'ID',
+      name: 'id'
+    },
+    {
+      title: 'Имя',
+      name: 'name'
+    },
+    {
+      title: 'E-mail',
+      name: 'email'
+    },
+    {
+      title: 'Телефон',
+      name: 'phone',
+    },
+    {
+      title: 'О себе',
+      name: 'about',
+    },
+  ]
+}
 
+const companySection = {
+  title: 'Компания',
+  rows: [
+    {
+      title: 'Название',
+      name: 'company_name'
+    },
+    {
+      title: 'Направление работы компании',
+      name: 'company_business_type_id',
+      toFormat: (item: any) => {
+        return item?.company_business_type?.name_ru
+      }
+    },
+    {
+      title: 'Должность',
+      name: 'job_title'
+    },
+    {
+      title: 'Адрес',
+      name: 'company_address'
+    },
+    {
+      title: 'Индекс',
+      name: 'company_index'
+    },
+    {
+      title: 'Страна',
+      name: 'company_country',
+      toFormat: (item: any) => {
+        return item?.company_country?.name_ru;
+      }
+    },
+    {
+      title: 'Город',
+      name: 'company_city'
+    },
+    {
+      title: 'Сайт компании',
+      name: 'company_url'
+    },
+    {
+      title: 'Добавлен',
+      name: 'created_at',
+      preset: { name: 'timestampToFormatPreset' }
+    },
+    {
+      title: 'Изменен',
+      name: 'updated_at',
+      preset: { name: 'timestampToFormatPreset' }
+    },
+  ]
+}
+
+const subscriptionSection = {
+  title: 'Подписка',
+  rows: [
+    {
+      title: 'Тип',
+      name: 'subscription_type',
+      component: Badge,
+      toFormat(item: IItem) {
+
+        const mapper: Record<string, string> = {
+          NONE: '--default',
+          ONLY_MUSIC: '--success',
+          ONLY_SOUNDS: '--success',
+          MUSIC_AND_SOUNDS: '--success'
+        }
+
+        return {
+          title: item?.subscription_type?.title,
+          class: mapper[item?.subscription_type?.id]
+        }
+      },
+    },
+    {
+      name: 'subscription_till',
+      title: 'Дата окончания подписки',
+      toFormat(item) {
+        if (!item.subscription_till) {
+          return '-'
+        }
+
+        return item?.subscription_till
+      }
+    },
+  ]
+}
+
+const subscriptionExclusiveSection = {
+  title: 'Подписка на эксклюзивные треки',
+  rows: [
+    {
+      name: 'condition',
+      title: 'Условия',
+      class: 6,
+      toFormat(item) {
+        return 'Активна только при действующей подписке на музыку';
+      }
+    },
+    {
+      name: 'subscription_till_for_exclusive_tracks',
+      title: 'Дата окончания подписки',
+      class: 6,
+      toFormat(item) {
+        if (!item.subscription_till_for_exclusive_tracks) {
+          return '-'
+        }
+
+        return item?.subscription_till_for_exclusive_tracks
+      }
+    },
+  ]
+}
+
+const sections = ref([
+  infoSection,
+  companySection,
+  subscriptionSection,
+  subscriptionExclusiveSection
 ])
 
 await SSRLoadDetail(item, 'users/detail', route.params.id)
@@ -28,7 +174,7 @@ await SSRLoadDetail(item, 'users/detail', route.params.id)
   <Detail
       :sections="sections"
       :item="item"
-      back-link="users"
+      back-link="/users"
       entity-name="Пользователь"
       @itemUpdated="onItemUpdated"
   >
@@ -39,11 +185,11 @@ await SSRLoadDetail(item, 'users/detail', route.params.id)
           modalComponent: UserForm,
           modalTitle: 'Изменение пользователя',
           notificationMessage: 'Пользователь изменен'
-        })" :class="['--big --outline-primary']">Изменить</Button>
+        })" :class="['--small --outline-primary']">Изменить</Button>
         <Button @click="onClickDelete({
           deleteURL: 'users/delete',
           notificationMessage: 'Пользователь удален'
-        })" :class="['--big --outline-danger']">Удалить</Button>
+        })" :class="['--small --outline-danger']">Удалить</Button>
       </div>
     </template>
   </Detail>
