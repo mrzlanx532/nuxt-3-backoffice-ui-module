0a15f87d82b191a0c008b1d0ff9a7e276456349f
Denis Danilov <d.danilov@duotek.ru>
2025-01-06 16:30:43 +0300

    Try to export types

--- src/runtime/components/Browser/Filters/BrowserSelectSearchFilter.vue
+++ src/runtime/components/Browser/Filters/BrowserSelectSearchFilter.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { IFilter } from '#backoffice-ui/components/Browser.vue'
-import { type Ref, ref, watch, onMounted, onUnmounted } from 'vue'
+import { type Ref, ref, watch, onMounted, onUnmounted, useTemplateRef } from 'vue'
 
 interface IOption {
   id: number|string,
